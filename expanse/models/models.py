a6560cbebcb6baab8a6078183a5dccf2e5434e6f
xxjcaxx <jocasal@gmail.com>
2022-10-04 10:53:28 +0000

    planets...

--- expanse/models/models.py
+++ expanse/models/models.py
@@ -9,3 +9,26 @@ class player(models.Model):
 
      name = fields.Char(required=True)
      password = fields.Char()
+     avatar = fields.Image(max_width = 200, max_height=200)
+     colonies = fields.One2many('expanse.colony','player')
+
+class planet(models.Model):
+     _name = 'expanse.planet'
+     _description = 'Planets'
+
+     name = fields.Char(required=True)
+     image = fields.Image(max_width = 200, max_height=200)
+     size = fields.Float()
+     colonies = fields.One2many('expanse.colony', 'planet')
+
+class colony(models.Model):
+     _name = 'expanse.colony'
+     _description = 'Colonies'
+
+     name = fields.Char(required=True)
+     planet = fields.Many2one('expanse.planet')
+     player = fields.Many2one('expanse.player')
+
+class spaceship(models.Model):
+     _name = 'expanse.spaceship'
+     _description = 'Spaceships'
