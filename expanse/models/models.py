816c3cc34c1a4bf88a04b7ae8ffa3c5c9d89ab49
xxjcaxx <jocasal@gmail.com>
2022-10-11 10:52:43 +0000

    dimarts

--- expanse/models/models.py
+++ expanse/models/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import random
+from odoo.exceptions import ValidationError
 
 class player(models.Model):
      _name = 'expanse.player'
@@ -11,6 +12,12 @@ class player(models.Model):
      password = fields.Char()
      avatar = fields.Image(max_width = 200, max_height=200)
      colonies = fields.One2many('expanse.colony','player')
+     colonies_qty = fields.Integer(compute="get_colonies_qty")
+
+     @api.depends('colonies')
+     def get_colonies_qty(self):
+         for p in self:
+             p.colonies_qty = len(p.colonies)
 
 class planet(models.Model):
      _name = 'expanse.planet'
@@ -19,7 +26,19 @@ class planet(models.Model):
      name = fields.Char(required=True)
      image = fields.Image(max_width = 200, max_height=200)
      size = fields.Float()
+     gravity = fields.Float(compute='_get_gravity')
      colonies = fields.One2many('expanse.colony', 'planet')
+
+     @api.constrains('size')
+     def check_planet_size(self):
+          for planet in self:
+               if planet.size > 1000 or planet.size < 1:
+                    raise ValidationError("Your planet is too big: %s" % planet.size)
+
+     @api.depends('size')
+     def _get_gravity(self):
+         for planet in self:
+             planet.gravity = 1
 
 class colony(models.Model):
      _name = 'expanse.colony'
@@ -28,7 +47,31 @@ class colony(models.Model):
      name = fields.Char(required=True)
      planet = fields.Many2one('expanse.planet')
      player = fields.Many2one('expanse.player')
+     player_avatar = fields.Image(related="player.avatar")
+     creation_date = fields.Datetime(default = fields.Datetime.now)
+     buildings = fields.One2many('expanse.building','colony')
 
 class spaceship(models.Model):
      _name = 'expanse.spaceship'
      _description = 'Spaceships'
+
+     name = fields.Char()
+
+class building(models.Model):
+     _name = 'expanse.building'
+     _description = 'Buildings'
+
+     name = fields.Char()
+     type = fields.Many2one('expanse.building_type')
+     level = fields.Integer(default=1)
+     colony = fields.Many2one('expanse.colony')
+     price_base = fields.Float(related='type.price_base')
+
+class building_type(models.Model):
+     _name = 'expanse.building_type'
+     _description = 'Building types'
+
+     name = fields.Char()
+     price_base = fields.Float()
+     water_production = fields.Float()
+
